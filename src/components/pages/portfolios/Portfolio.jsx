290fad7fcb74b30f3e051c6babca266ffb3ab3a1
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-07-27 14:10:04 +0530

    2 files modified

--- src/components/pages/portfolios/Portfolio.jsx
+++ src/components/pages/portfolios/Portfolio.jsx
@@ -7,6 +7,241 @@ export default function Portfolio() {
   return (
     <div>
       <Hero/>
+      <div className="portfolio_blog">
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/oncq-desk.png" className="w-100" alt=""/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/lpl-tab.png" className="w-100" alt=""/>
+          </Col>
+
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+      
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/admissify-desk.png" className="w-100" alt=""/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/hsbp-mob1.png" className="w-100" alt=""/>
+          </Col>
+
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+      {/* sadfc */}
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/gocontest-desk.png" className="w-100" alt=""/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/vcare-desk.png" className="w-100" alt=""/>
+          </Col>
+
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+        {/* sadfc */}
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/divinebeauty-desk.png" className="w-100" alt=""/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/organic2.png" className="w-100" alt=""/>
+          </Col>
+
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+
+        {/* sadfc */}
+        <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/globe-desk.png" className="w-100" alt=""/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
+    <section>
+      <Container className="py-5">
+        <Row>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/holisol-desk.png" className="w-100" alt=""/>
+          </Col>
+
+          <Col lg={6}>
+            <h3>ONCQUEST</h3>
+            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            {/* <br/> */}
+            <br/>
+            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+
+            <div className="portfolio_hero_btn">
+              <button>CASE STUDY</button>
+              <button className="mx-4">CASE STUDY</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+      
+
+    </div>
+
 
     
     
