c9609b336382ec3272c090a2f63be62c97a4a1ff
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-07-27 20:14:14 +0530

    3 files modified

--- src/components/pages/products/Hero.jsx
+++ src/components/pages/products/Hero.jsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React,{useState} from "react";
 import { Container, Row,Col } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
+
 
 export default function Hero() {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+
   return (
     <div>
-      <section className="protfolio_hero">
+      <Carousel data-bs-theme="dark">
+      <Carousel.Item className="carousel_caption">
+        <img
+          className="d-block w-100"
+          src="https://cdn.pixabay.com/photo/2015/06/22/08/37/children-817365_640.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption className="caption_caousel">
+          <h5>Empowering</h5>
+          <h1>User <br/> Statifaction</h1>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum pharetra augue mollis interdu.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item className="carousel_caption">
+        <img
+          className="d-block w-100"
+          src="https://cdn.pixabay.com/photo/2016/10/06/05/19/couple-1718244_640.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h5>Second slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="carousel_caption">
+        <img
+          className="d-block w-100"
+          src="https://media.istockphoto.com/id/1400628256/photo/paris-silhouette-of-couple-kissing-near-eiffel-tower.webp?b=1&s=612x612&w=0&k=20&c=5nXLDv77PLWmNMoWkWGB24sAK4aYlqQC01TZd42iEzA="
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h5>Third slide label</h5>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+      {/* <section className="protfolio_hero">
       <Container>
         <Row>
           <Col lg={6} className="portfolio_hero_content">
@@ -18,7 +66,7 @@ export default function Hero() {
           </Col>
         </Row>
       </Container>
-      </section>
+      </section> */}
 
     </div>
   );
