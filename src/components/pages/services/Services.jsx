ad70b9fecb34c57b59de19e1146bcd77ede59f57
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-07-29 03:17:26 +0530

    3 files modified

--- src/components/pages/services/Services.jsx
+++ src/components/pages/services/Services.jsx
@@ -11,7 +11,8 @@ export default function Services() {
       <Tab.Container id="left-tabs-example" defaultActiveKey="first">
       <Row >
         <Col lg={3}  >
-         <div className="lift_sidemenu">
+        <div className="lift_sidemenu">
+           <h3>Our Services</h3>
          <Nav variant="pills" className="flex-column">
             <Nav.Item>
               <Nav.Link eventKey="first">Design </Nav.Link>
@@ -26,7 +27,7 @@ export default function Services() {
               <Nav.Link eventKey="four">IT CONSULTING SERVICES </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="fiver">Devops</Nav.Link>
+              <Nav.Link eventKey="five">Devops</Nav.Link>
             </Nav.Item>
           </Nav>
          </div>
@@ -34,8 +35,7 @@ export default function Services() {
         <Col lg={9}  className="side_right_content">
           <Tab.Content>
             <Tab.Pane eventKey="first">
-          
-            <Row className="p-2">
+            <Row  className="side_content_text p-2">
              <Col lg={3} className="">
                <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
                <h5>User Experience Design</h5>
@@ -56,29 +56,131 @@ export default function Services() {
                <h5>User Experience Design</h5>
                <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
              </Col>
-             
             </Row>  
-         
-
             </Tab.Pane>
-            <Tab.Pane eventKey="second">
-            <Row className="">
-              <Col lg={8} className="">
-                <p>“AbyM Technology” is a leading IT and digital solution provider company that helps businesses grow and succeed in the online world.</p>
-                <br/>
-                <p>“AbyM” was founded in 2010 by a team of passionate and experienced IT professionals who wanted to create innovative and effective solutions for various industries. AbyM Technology has been providing high-quality and reliable services ever since, such as web development, app development, digital marketing, graphic design, content writing, and more. AbyM Technology has been the trusted partner of many clients across the globe, ranging from small startups to large corporations.</p>
 
-                <div className="about_right_btn">
-                  <button className="comp_btn">Know more about</button>
-                  <button className="mx-3">Introduction</button>
-                </div>
-
-              </Col>
-              <Col lg={4} className="">
-                <img src="https://abym.in/assets/img/banner/brand-drop.png" className="w-100" alt="" />
-              </Col>
+          <Tab.Pane eventKey="second">
+           <Row className="side_content_text">
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>Development & Operations</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>Mobile App Development</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>CRM Development</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>Software / Custom Development</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             </Row>
+             <Row className="side_content_text mt-3">
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>Development & Operations</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>Mobile App Development</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>CRM Development</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>Software / Custom Development</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
             </Row>
+           </Tab.Pane>
+
+            <Tab.Pane eventKey="three">
+            <Row className="side_content_text">
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+            </Row>  
             </Tab.Pane>
+
+            <Tab.Pane eventKey="four">
+            <Row className="side_content_text">
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+            </Row>  
+            </Tab.Pane>
+
+            <Tab.Pane eventKey="five">
+            <Row className="side_content_text">
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+             <Col lg={3} className="">
+               <img src="https://abym.in/assets/img/icon/service_ico4.png" alt=""/>
+               <h5>User Experience Design</h5>
+               <p>Discovery & Application AssessmentBusiness Rules MiningRe-Platform ServicesMigration Services</p>
+             </Col>
+            </Row>  
+            </Tab.Pane>
+
           </Tab.Content>
         </Col>
       </Row>
