2b9e68db86fe5a46872b55bce5d1eb49e8817f6f
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-07-30 03:39:14 +0530

    2 files modified

--- src/components/pages/home/About.jsx
+++ src/components/pages/home/About.jsx
@@ -12,11 +12,11 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 export default function About() {
   return (
     <>
-    <div className="py-5 my-5">
+    <div className="py-5 my-5 home_trusted_partner">
       <Container>
         <Row>
           <Col lg={6}>
-            <img src="https://cdn.pixabay.com/photo/2015/10/31/12/00/meeting-1015313_640.jpg" className="w-100" alt=""/>
+            <img src="https://abym.in/assets/img/post/1.png" className="w-100" alt=""/>
           </Col>
           <Col lg={6}>
             <h5 className="">WHO WE ARE</h5>
@@ -24,21 +24,21 @@ export default function About() {
             <h1>IT Industry</h1>
             <p>AbyM Technology is a certified and award winning IT company. We have been acclaimed for our IT work since 2010. We deliver the best IT solutions in Android, iOS, and web development.</p>
             <article>
-              <div className="mt-5 pb-5">
+              <div className="mt-5 pb-5 home_trusted_partner_decs">
               <Row>
-                <Col lg={3}>
-                  <img src="https://abym.in/assets/img/icon/certificate-icon.png" className="" style={{width:"120px", height:"90px"}} alt=""/>
+                <Col lg={2}>
+                  <img src="https://abym.in/assets/img/icon/certificate-icon.png" alt=""/>
                 </Col>
-                <Col lg={9}>
+                <Col lg={10}>
                   <h4>Certified Company</h4>
                   <p>Certified IT company with proven quality, security, speed. Best IT solutions for your business.</p>
                 </Col>
               </Row>
               <Row className="mt-3">
-                <Col lg={3}>
-                  <img src="https://abym.in/assets/img/icon/award-icon.png"  className="" style={{width:"120px", height:"90px"}} alt=""/>
+                <Col lg={2}>
+                  <img src="https://abym.in/assets/img/icon/award-icon.png" alt=""/>
                 </Col>
-                <Col lg={9}>
+                <Col lg={10}>
                   <h4>Award Winning</h4>
                   <p>Certified IT company with proven quality, security, speed. Best IT solutions for your business.</p>
                 </Col>
@@ -54,7 +54,7 @@ export default function About() {
       </Container>
     </div>
 
-    <section className="bg-secondary">
+    <section className="home_working_process">
      <Container>
       <Heading totitle="PROCESS" title="Our Working Process" subtitle="As a website-building firm, we are constantly attempting to innovate and experiment with new layouts. Modernization is something we like. We can design an excellent website for you that is creative, appealing, informational, and efficient."  />
         <Row className="text-center py-0">
