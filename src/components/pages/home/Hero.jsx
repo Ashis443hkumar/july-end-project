4bada3296360e8dae1bb66c5fd8787293e17bd52
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-07-30 02:57:08 +0530

    3 files modified

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
       <Carousel.Item className="carousel_caption">
         <img
           className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2015/06/22/08/37/children-817365_640.jpg"
+          src="https://abym.in/assets/img/banner/banner3.png"
           alt="First slide"
         />
         <Carousel.Caption className="caption_caousel">
@@ -27,12 +27,12 @@ export default function Hero() {
       <Carousel.Item className="carousel_caption">
         <img
           className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2016/10/06/05/19/couple-1718244_640.jpg"
+          src="https://abym.in/assets/img/banner/banner2.png"
           alt="Second slide"
         />
         <Carousel.Caption className="caption_caousel">
-          <h5>Empowering 2</h5>
-          <h1>User <br/> Statifaction</h1>
+          <h5>Transforming</h5>
+          <h1>Coustomer <br/> Engagement</h1>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum pharetra augue mollis interdu.</p>
           <br/>
           <button className="comp_btn">Consult Our Team <i class="fa-solid fa-arrow-right"></i></button>
@@ -41,13 +41,17 @@ export default function Hero() {
       <Carousel.Item className="carousel_caption">
         <img
           className="d-block w-100"
-          src="https://media.istockphoto.com/id/1400628256/photo/paris-silhouette-of-couple-kissing-near-eiffel-tower.webp?b=1&s=612x612&w=0&k=20&c=5nXLDv77PLWmNMoWkWGB24sAK4aYlqQC01TZd42iEzA="
+          src="https://abym.in/assets/img/banner/banner1.png"
           alt="Third slide"
         />
          <Carousel.Caption className="caption_caousel">
-          <h5>Empowering 3</h5>
-          <h1>User <br/> Statifaction</h1>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum pharetra augue mollis interdu.</p>
+           
+						
+							
+          <h5>Redefining</h5>
+          <h1>User <br/> Expriences</h1>
+          <p>Our innovative technology and creative expertise empower us to transform your vision
+						into a tangible solution.</p>
           <br/>
           <button className="comp_btn">Consult Our Team <i class="fa-solid fa-arrow-right"></i> </button>
         </Carousel.Caption>
