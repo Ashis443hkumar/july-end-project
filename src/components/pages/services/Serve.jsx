855b5c31a159e6630b13f09554caa3fe7177ac77
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-07-29 05:54:25 +0530

    1 file modified

--- src/components/pages/services/Serve.jsx
+++ src/components/pages/services/Serve.jsx
@@ -81,12 +81,87 @@ export default function Serve() {
             </div>
               <button>VALIDATE YOUR PRODUCT IDEA</button>
           </Col>
-
-          
         </Row>
       </Container>
       </section>
 
+      <section className="serve_banner py-3" style={{background:"aliceblue"}}>
+      <Container>
+        <Row>
+          <Col lg={7} className="pt-5">
+           <div className="pt-5 text-center">
+           <h1>App Development</h1>
+            <p>Fast, reliable, user-friendly apps for Android, iOS, or both. Latest technologies and best practices. App maintenance and support. Free consultation and quote.</p>
+           </div>
+          </Col>
+          <Col lg={5}>
+            <img src="https://abym.in/assets/img/post/ser-dev-add.png" className="w-100" alt=""/>
+          </Col>
+        </Row>
+      </Container>
+      </section>
+      
+      <section className="abc my-5 py-5">
+      <Container>
+        <Row>
+          <Col lg={6}>
+            <h4>DIGITAL MARKETING</h4>
+            <h5>All-in-One Digital Marketing for Youk</h5>
+            <p>AbyM Technology offers digital marketing services to help you grow your online business. We provide SEO, social media, email, content, and PPC solutions tailored to your goals and budget. Boost your brand, leads, and sales with us.</p>
+
+            <h5>Get Online Marketing Success With Us</h5>
+            <div className="dd mt-4">
+              <span>Search Engine Optimization</span>
+              <span className="px-5 mx-5"><img  className="px-5" src="https://abym.in/assets/img/icon/explore-black.png" alt=""/></span>
+            </div>
+            <div className="dd my-4">
+              <span>Social Media Marketing</span>
+              <span className="mx-5 px-5"><img  className="px-5" src="https://abym.in/assets/img/icon/explore-black.png" alt=""/></span>
+            </div>
+            <div className="dd mb-5">
+              <span>Email Marketing</span>
+              <span className="px-5 mx-5"><img  className="px-5" src="https://abym.in/assets/img/icon/explore-black.png" alt=""/></span>
+            </div>
+              <button>VALIDATE YOUR PRODUCT IDEA</button>
+          </Col>
+          <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/digital-post.png" className="w-100" alt=""/>
+        </Col>
+        </Row>
+      </Container>
+      </section>
+
+
+      <section className="abc my-5 py-5">
+      <Container>
+        <Row>
+        <Col lg={6}>
+            <img src="https://abym.in/assets/img/post/it-consult-post.png" className="w-100" alt=""/>
+        </Col>
+
+          <Col lg={6}>
+            <h4>IT CONSULTING SERVICES</h4>
+            <h5>Professional Guidance for Your IT Challenges</h5>
+            <p>Get IT consulting services from AbyM Technology. We help you with IT infrastructure, processes, solutions, and risks. Improve your IT performance and security with us.</p>
+
+            <h5>Our Ideation & Design Services</h5>
+            <div className="dd mt-4">
+              <span>IT infrastructure consulting</span>
+              <span className="px-5 mx-5"><img  className="px-5" src="https://abym.in/assets/img/icon/explore-black.png" alt=""/></span>
+            </div>
+            <div className="dd my-4">
+              <span>IT process optimization</span>
+              <span className="mx-5 px-5"><img  className="px-5" src="https://abym.in/assets/img/icon/explore-black.png" alt=""/></span>
+            </div>
+            <div className="dd mb-5">
+              <span>IT solution implementation</span>
+              <span className="px-5 mx-5"><img  className="px-5" src="https://abym.in/assets/img/icon/explore-black.png" alt=""/></span>
+            </div>
+              <button>VALIDATE YOUR PRODUCT IDEA</button>
+          </Col>
+        </Row>
+      </Container>
+      </section>
       
 
       
