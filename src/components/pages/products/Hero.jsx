36f12bc946d199900e974edb4115e29a2927c4c9
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-08-17 16:49:02 +0530

    3 files modified

--- src/components/pages/products/Hero.jsx
+++ src/components/pages/products/Hero.jsx
@@ -1,59 +1,26 @@
-import React,{useState} from "react";
+import React from "react";
 import { Container, Row,Col } from "react-bootstrap";
-import Carousel from 'react-bootstrap/Carousel';
 
+import "./style.css"
 
 export default function Hero() {
-
   return (
     <div>
-      <Carousel data-bs-theme="dark">
-      <Carousel.Item className="carousel_caption">
-        <img
-          className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2015/06/22/08/37/children-817365_640.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption className="caption_caousel">
-          <h5>Empowering 1</h5>
-          <h1>User <br/> Statifaction</h1>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum pharetra augue mollis interdu.</p>
-          <br/>
-          <button className="comp_btn">Consult Our Team <i class="fa-solid fa-arrow-right"></i></button>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item className="carousel_caption">
-        <img
-          className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2016/10/06/05/19/couple-1718244_640.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption className="caption_caousel">
-          <h5>Empowering 2</h5>
-          <h1>User <br/> Statifaction</h1>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum pharetra augue mollis interdu.</p>
-          <br/>
-          <button className="comp_btn">Consult Our Team <i class="fa-solid fa-arrow-right"></i></button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item className="carousel_caption">
-        <img
-          className="d-block w-100"
-          src="https://media.istockphoto.com/id/1400628256/photo/paris-silhouette-of-couple-kissing-near-eiffel-tower.webp?b=1&s=612x612&w=0&k=20&c=5nXLDv77PLWmNMoWkWGB24sAK4aYlqQC01TZd42iEzA="
-          alt="Third slide"
-        />
-         <Carousel.Caption className="caption_caousel">
-          <h5>Empowering 3</h5>
-          <h1>User <br/> Statifaction</h1>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum pharetra augue mollis interdu.</p>
-          <br/>
-          <button className="comp_btn">Consult Our Team <i class="fa-solid fa-arrow-right"></i> </button>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-     
+      <section className="protfolio_hero1  products_hero2 ">
+      <Container>
+        <Row>
+          <Col lg={6} className="portfolio_hero_content">
+            <h4>Boost Your Business with</h4>
+            <h1>Innovative  <br/> Solutions</h1>
+            <hr/>
+            <p>You deserve the best. That’s why we offer you smart, customized, and effective solutions to meet your needs. Whether you want to grow your online presence, optimize your website, or reach new customers, we have the right tools for you.</p>
+          </Col>
+          <Col lg={6} className="product_img p-5 pt-0">
+            <img src="https://abym.in/assets/img/banner/product-ban-img.png" className="" alt="" />
+          </Col>
+        </Row>
+      </Container>
+      </section>
 
     </div>
   );
