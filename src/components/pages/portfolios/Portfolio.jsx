f4e5e98e0b1f9f063f73c914c73852024936f71b
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-08-21 19:46:33 +0530

    3 files modified

--- src/components/pages/portfolios/Portfolio.jsx
+++ src/components/pages/portfolios/Portfolio.jsx
@@ -9,24 +9,21 @@ export default function Portfolio() {
     <div>
       <Hero/>
       <div className="portfolio_blog">
-
-     <section>
+      <section className="">
       <Container className="py-5">
         <Row>
           <Col lg={6}>
             <h3>ONCQUEST</h3>
             <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
             {/* <br/> */}
-            <br/>
             <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
-
             <div className="portfolio_hero_btn">
-              <button>CASE STUDY</button>
-              <button className="mx-4">CASE STUDY</button>
+              <button className="hero_btn_border">CASE STUDY </button>
+              <button className="mx-4 hero_btn_outline">VISIT WEBSITE </button>
             </div>
           </Col>
           <Col lg={6}>
-            <img src="https://abym.in/assets/img/post/oncq-desk.png" className="w-100" alt=""/>
+            <img src="https://abym.in/assets/img/post/oncq-desk.png" className="" alt=""/>
           </Col>
         </Row>
       </Container>
@@ -40,16 +37,13 @@ export default function Portfolio() {
           </Col>
 
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            <h3>LALPATHLABS</h3>
+            <p>When it comes to your health, you deserve the best. That’s why Dr Lal Path Labs is your trusted partner for all your diagnostic and preventive healthcare needs. They have a huge network of labs, centres, and pick-up points all over India. They make it easy and convenient for you to book tests, download reports, and access wellness packages online.</p>
             {/* <br/> */}
-            <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            
+            <p>AbyM Technology is proud to have worked with them on their website and app development, content creation, design, and other IT solutions. We have built a stunning and secure online platform that brings patients, doctors, and labs closer together. We have also made sure their tests and reports are reliable and accurate. We have given their website and app a fresh and user-friendly look.</p>
 
-            <div className="portfolio_hero_btn">
-              <button>CASE STUDY</button>
-              <button className="mx-4">CASE STUDY</button>
-            </div>
+        
           </Col>
         </Row>
       </Container>
@@ -60,11 +54,9 @@ export default function Portfolio() {
       <Container className="py-5">
         <Row>
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
-            {/* <br/> */}
-            <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <h3>ADMISSIFY</h3>
+            <p>Admissify is a UK & India-based company that helps students achieve their dreams of studying abroad. They offer guidance and assistance for applying to over 3000+ top universities across the world. They also provide services such as alumni chat, scholarship, immigration, and job support..</p>
+            <p>AbyM Technology has done its website design and development, app design and development, content development and other IT works. We have created a stunning and user-friendly online platform that simplifies the application process and connects students with counsellors and experts. We have also ensured the security and privacy of the users and their data. We have given their website and app a fresh and attractive look. </p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -86,11 +78,11 @@ export default function Portfolio() {
           </Col>
 
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            <h3>HSBP SALON</h3>
+            <p>HSBP Salon is a home service beauty parlour that offers salon and beauty services at your doorstep. They have professional and experienced beauticians, hair stylists and makeup artists who can pamper you with services like waxing, facial, manicure, pedicure, hair spa, makeup, hair style, bridal makeup and more..</p>
             {/* <br/> */}
             <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <p>AbyM Technology has created a stunning and user-friendly website and app for them that allows you to book appointments, chat with experts and access offers easily. We have also ensured the security and privacy of their data. We have given their website and app a new and appealing look.</p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -106,11 +98,11 @@ export default function Portfolio() {
       <Container className="py-5">
         <Row>
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            <h3>GO CONTEST</h3>
+            <p>GoContest is India's top digital contest platform, offering enjoyable and lucrative competitions and tournaments for a range of subjects, including GK, maths, history, and more. Users may easily and swiftly play, win, and get rewards and prizes. Website development, app development, content creation, web/app design, and other IT tasks have all been completed by AbyM Technology.</p>
             {/* <br/> */}
             <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <p>We have developed an amazing and captivating online platform that highlights the best in inventiveness and creativity. Additionally, we have made sure that both users' and developers' data is secure and private. We improved the look and usability of their website and mobile app. </p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -132,11 +124,11 @@ export default function Portfolio() {
           </Col>
 
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            <h3>VCARE</h3>
+            <p>VCare is a pioneer in digital health solutions, empowering users and providers to manage and access their health records anytime, anywhere. VCare integrates with the Ayushman Bharat Digital Mission (ABDM) to provide a secure and convenient platform for personal health records (PHR). VCare also offers a comprehensive clinic management system for doctors and providers, helping them improve productivity and health outcomes.</p>
             {/* <br/> */}
             <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <p>AbyM Technology developed VCare’s website and lead generation campaigns, showcasing its innovative and user-centric features. We are proud to partner with VCare in their mission to transform healthcare in India. </p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -152,11 +144,10 @@ export default function Portfolio() {
       <Container className="py-5">
         <Row>
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
-            {/* <br/> */}
-            <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <h3>DIVINEBEAUTY</h3>
+            <p>We created a stunning website for Divine Beauty, a national beauty pageant in India that selects the country’s representatives for Miss Earth and other international contests.</p>
+            
+            <p>We used advanced tools and database management to showcase their vision, mission, history, achievements, and events. We also optimized their website for organic searches, increasing their online visibility and reach. </p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -178,11 +169,11 @@ export default function Portfolio() {
           </Col>
 
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            <h3>ORGANIC KITCHEN</h3>
+            <p>We designed and developed a user-friendly website for Organic Kitchen, a brand that delivers organic homestyle food in Delhi & NCR. We integrated features such as weekly menu, corporate catering, tiffin services, food lab test reports, and online ordering.</p>
             {/* <br/> */}
             <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <p>We also ensured that their website ranks high on organic searches, attracting more customers and generating more revenue. </p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -199,11 +190,10 @@ export default function Portfolio() {
       <Container className="py-5">
         <Row>
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
-            {/* <br/> */}
-            <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <h3>GLOBE</h3>
+            <p>GLOBE is a cancer hospital in Lucknow that offers top-quality care to patients with advanced diagnostics and clinical services. GLOBE has a team of expert cancer specialists who are committed to transforming cancer care and improving patient outcomes. GLOBE provides a safe, comfortable, and accessible environment for patients and their families.</p>
+            
+            <p>AbyM Technology helped GLOBE to design its UI/UX and manage patients' databases, enhancing its efficiency and performance. We are proud to partner with GLOBE in their noble cause of fighting cancer. </p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
@@ -225,11 +215,11 @@ export default function Portfolio() {
           </Col>
 
           <Col lg={6}>
-            <h3>ONCQUEST</h3>
-            <p>Oncquest helps people stay healthy and detect diseases early. They have the best tests and screenings for cancer, diabetes, heart disease, and more.</p>
+            <h3>HOLISOL</h3>
+            <p>HOLISOL is a supply chain organization that offers technology-enabled solutions for various industries. HOLISOL helps customers optimize their operations, reduce costs, and enhance customer satisfaction. HOLISOL has a team of passionate and experienced supply chain professionals who deliver value through innovation and leadership.</p>
             {/* <br/> */}
             <br/>
-            <p>AbyM Technology is happy to have worked with them on their website, multilingual mobile app, patient database CMS and other IT solutions. We made their online platform easy and safe to use for patients, doctors, and labs all over India. We wanted to make their website more attractive and user-friendly. </p>
+            <p>AbyM Technology worked with HOLISOL to design their UI/UX and manage their database, ensuring a smooth and user-friendly experience for their clients and partners. We are delighted to support HOLISOL in its mission to create smart and sustainable supply chains.</p>
 
             <div className="portfolio_hero_btn">
               <button>CASE STUDY</button>
